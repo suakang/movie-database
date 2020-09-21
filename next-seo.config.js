783f5a683f6318dab44843d5250f04bf84f0100a
SuA Kang <sua@suakang.net>
2020-09-21 16:06:16 -0400

    Initial commit

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+    title: "Movie Database",
+    description: "Database of movies created with Next.js and Strapi",
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://www.nextmovies.test',
+        site_name: 'Movie Database',
+    },
+    twitter: {
+        handle: '@ivan_doric',
+        site: '@WatchLearnTuts',
+        cardType: 'summary_large_image',
+    },
+};
